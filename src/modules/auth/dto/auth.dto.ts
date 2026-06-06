f4cca08e954d8c46aeb21f808f0e78a44c29950d
muhammed-codes <dimeji.asynctechs@gmail.com>
2026-06-06 10:59:34 +0100

    refactor: update AuthCredentialsDto and SignUpDto examples with generic and updated placeholder values

--- src/modules/auth/dto/auth.dto.ts
+++ src/modules/auth/dto/auth.dto.ts
@@ -2,18 +2,18 @@ import { IsEmail, IsString, MinLength, IsOptional } from "class-validator";
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 
 export class AuthCredentialsDto {
-  @ApiProperty({ example: "bellomuhammedoladimeji@gmail.com" })
+  @ApiProperty({ example: "[EMAIL_ADDRESS]" })
   @IsEmail()
   email!: string;
 
-  @ApiProperty({ example: "Atinuke@top1" })
+  @ApiProperty({ example: "P@ssw0rd" })
   @IsString()
   @MinLength(6)
   password!: string;
 }
 
 export class SignUpDto extends AuthCredentialsDto {
-  @ApiProperty({ example: "John Doe" })
+  @ApiProperty({ example: "Muhammed Doe" })
   @IsString()
   displayName!: string;
 
@@ -22,7 +22,7 @@ export class SignUpDto extends AuthCredentialsDto {
   @IsString()
   profilePhoto?: string;
 
-  @ApiPropertyOptional({ example: "Software Engineer from Lagos" })
+  @ApiPropertyOptional({ example: "Software Engineer from Lagun" })
   @IsOptional()
   @IsString()
   bio?: string;
